beab5369671569b3b2ef6383fd71cd837443a028
cho-hazel <hcho3@ualberta.ca>
2026-09-01 22:32:09 -0600

    Add alternative plant matches and comparison (#24)
    Co-authored-by: cho-hazel <hcho3@ualberta>

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  agentRules: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bs.plantnet.org",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
